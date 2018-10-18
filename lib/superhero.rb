c84bf7ee34be0840e4e05b1f7449698c1f179bf6
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-18 11:40:10 +0100

    Added hash of DOB superhero names

--- lib/superhero.rb
+++ lib/superhero.rb
@@ -1,22 +1,31 @@
 class Superhero
 
-  attr_reader :first_initial
+  attr_reader :first_initial, :last_initial, :day
 
   def initialize
-    @first_initial = {a: 'The Dope', b: 'The Brilliant', c: 'The Magical',
-    d: 'The Fantastic', e: 'The Bobastic', f: 'The Mighty', g: 'The Outlandish',
-    h: 'The Fearless', i: 'The Invicible', j: 'The Radioactive', k: 'The Dark',
-    l: 'The Daring', m: 'The Unstoppable', n: 'The Frightening', o: 'The Mega',
-    p: 'The Galactic', q: 'The Impossible', r: 'The Howling', s: 'The Weird',
-    t: 'The Incredible', u: 'The Mysterious', v: 'The Untouchable',
-    w: 'The Sensational', x: 'The Dazzling', y: 'The Wonderful', z: 'The Brainy'
+    @first_initial = {a: 'The Daring', b: 'The Brilliant', c: 'The Magical',
+      d: 'The Fantastic', e: 'The Wonderful', f: 'The Mighty', g: 'The Dark',
+      h: 'The Fearless', i: 'The Invicible', j: 'The Radioactive', k: 'The Hot',
+      l: 'The Crazy', m: 'The Unstoppable', n: 'The Frightening', o: 'The Mega',
+      p: 'The Galactic', q: 'The Impossible', r: 'The Howling', s: 'The Weird',
+      t: 'The Incredible', u: 'The Mysterious', v: 'The Untouchable',
+      w: 'The Sensational', x: 'The Dazzling', y: 'The Cold', z: 'The Brainy'
     }
 
     @last_initial = {a: 'Shrieking', b: 'Silver', c: 'Acidic', d: 'Gentle',
-    e: 'Red', f: 'Purple', g: 'Crystal', h: 'Metal', i: 'Pink', j: 'Shiny',
+    e: 'Red', f: 'Purple', g: 'Stinky', h: 'Metal', i: 'Pink', j: 'Shiny',
     k: 'Titanium', l: 'Electric', m: 'Flying', n: 'Strong', o: 'Paper',
-    p: 'Wooden', q: 'Tiny', r: 'Huge', s: 'Stinky', t: 'Yellow', u: 'Diamond',
+    p: 'Wooden', q: 'Tiny', r: 'Huge', s: 'Crystal', t: 'Yellow', u: 'Diamond',
     v: 'Orange', w: 'Fiery', x: 'Golden', y: 'Greasey', z: 'Masked'
+    }
+
+    @day = {1 => 'Doctor', 2 => 'Wizard', 3 => 'Warrior', 4 => 'Mutant',
+      5 => 'Ninja', 6 => 'Squirrel', 7 => 'Ghost', 8 => 'Dragon', 9 => 'Wolf',
+      10 => 'Soldier', 11 => 'Detective', 12 => 'Banana', 13 => 'Unicorn',
+      14 => 'Beast', 15 => 'Eagle', 16 => 'Elf', 17 => 'Cat', 18 => 'Goblin',
+      19 => 'Giant', 20 => 'Alien', 21 => 'Bug', 22 => 'Man', 23 => 'Hobbit',
+      24 => 'Dwarf', 25 => 'Pegasus', 26 => 'Giraffe', 27 => 'Hawk',
+      28 => 'Flame', 29 => 'Robot', 30 => 'Assassin', 31 => 'Ranger'
     }
   end
 
--- spec/superhero_spec.rb
+++ spec/superhero_spec.rb
@@ -16,15 +16,26 @@ describe Superhero do
 
   describe '#last_initial' do
 
-    it 'Should initialize with a hash of first initial titles' do
+    it 'Should initialize with a hash of last initial titles' do
       expect(subject.first_initial).to be_a(Hash)
     end
 
     it 'Should have a value for each letter' do
-      expect(subject.first_initial.length).to eq(26)
+      expect(subject.last_initial.length).to eq(26)
     end
 
   end
 
+  describe '#day' do
+
+    it 'Should initialize with a hash of day of birth titles' do
+      expect(subject.day).to be_a(Hash)
+    end
+
+    it 'Should have a value for each letter' do
+      expect(subject.day.length).to eq(31)
+    end
+
+  end
 
 end
